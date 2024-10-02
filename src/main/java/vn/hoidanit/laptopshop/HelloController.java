d8bcfc880eb318c2ce6f88177fe1973671b3f8fc
Dang The Hao <dangthehao28@gmail.com>
2024-10-02 22:35:54 +0700

    video 17

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,14 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello Spring Boot with Well";
+    }
+
+}
