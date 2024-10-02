75bc46d5c32b6c491e438a4781d8c524717b0b90
Dang The Hao <dangthehao28@gmail.com>
2024-10-02 23:01:35 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,7 +2,6 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
