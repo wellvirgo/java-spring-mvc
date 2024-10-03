1ba97e7af3235fe279020ce96403d6f6ae0ddf52
Dang The Hao <dangthehao28@gmail.com>
2024-10-03 22:50:09 +0700

    chap 7 bai 43

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello Spring Boot with Well";
+        return "Hello Spring Boot";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 
 }
